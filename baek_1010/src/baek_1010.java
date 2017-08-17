ef4de70cfa4e218ea9761caf17a8505c2eee7f38
종곤 <ljg9574@gmail.com>
2017-08-17 22:49:54 +0900

    수정

--- baek_1010/src/baek_1010.java
+++ baek_1010/src/baek_1010.java
@@ -1,3 +1,10 @@
+/*
+ * 2017.08.17
+ * BaekJoon 1010 : 조합 계산, dynamic programming
+ * @author gonjong
+ * 조합을 곱셈 계산으로 하면 부동소수점 문제로 오류값이 발생 ; 곱셈 방식은 미해결
+ */
+
 import java.util.Scanner;
 
 public class baek_1010 {
@@ -21,19 +28,39 @@ public class baek_1010 {
 			N = sc.nextInt();
 			M = sc.nextInt();
 			
-			System.out.println(combination(M, N));;
+			System.out.println(dp(M, N));
 		}
 	}
 	
+	long dp(int n, int m) {
+
+		long dp[][] = new long[n+1][n+1];
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=0;j<=i; j++) {
+				if(j==0||j==i) {
+					dp[i][j] = 1;
+				}
+				else {
+					dp[i][j] = dp[i-1][j-1]+dp[i-1][j];		
+				}
+			}
+		}
+		
+		
+		return dp[n][m];	
+		
+	}
+	
 	long combination(int n, int m) {
-		double x = 1;
-		double y = 1;
+		long x = 1;
+		long y = 1;
 	
 		for(int j=0; j<m; j++) {
 			x *= n-j;
 			y *= j+1;
 		}
 		
-		return (long)(x/y);
+		return (x/y);
 	}
 }
